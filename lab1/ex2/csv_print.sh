ff27e9e54fc591908cb2fa972432affd54635a4a
Andrei Cristian <andreicristian6@protonmail.com>
2019-03-02 08:45:17 +0200

    lab1 ex2 complete

--- lab1/ex2/csv_print.sh
+++ lab1/ex2/csv_print.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
 
+field=""
+
 if [[ $# -ne 1 ]]; then
 	echo "No argument given;"
 	echo "$(basename $0) file.csv"
 	exit 1
 fi
-
-for i in $(seq 1 5); do
-	echo $(cut -d , -f $i $1)
-done
+while read line; do
+	for i in $(seq 1 5); do
+		if [[ $i -eq "1" ]]; then
+			field="Host: "
+		elif [[ $i -eq "2" ]]; then
+			field="IP: "
+		elif [[ $i -eq "3" ]]; then
+			field="Web: "
+		elif [[ $i -eq "4" ]]; then
+			field="Backend: "
+		elif [[ $i -eq "5" ]]; then
+			field="Database: "
+		fi
+		echo $field$(echo $line | cut -d , -f $i)
+	done
+	echo --------------------
+done < $1
