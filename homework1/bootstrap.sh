de3050a05d03e33b567977fa25810814932cd153
Andrei Cristian <andreicristian6@protonmail.com>
2019-03-14 13:06:04 +0200

    update
    Signed-off-by: Andrei Cristian <andreicristian6@protonmail.com>

---
+++ homework1/bootstrap.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#update apps;
+yum -y update
+
+#SSH CONFIG
+if [[ $(find /root/.ssh | wc -l) -ne 1 ]]; then
+	mkdir /root/.ssh;
+fi
+
+SCF_PATH="/tmp/MLMOS/homework1/ssh_cfg"
+PUB_KEY=$SCF_PATH/id_mlmos.pub
+SSH_PATH="/etc/ssh"
+
+cp $PUB_KEY /root/.ssh/
+cp $SSH_PATH/sshd_config $SSH_PATH/sshd_config.bk
+rm -f $SSH_PATH/sshd_config
+cp $SCF_PATH/sshd_config $SSH_PATH/sshd_config
+
+#SELINUX
+SEL_PATH="/etc/selinux"
+if [[ $(grep 'SELINUX=enforcing' $SEL_PATH/config | wc -l) -ne 0 ]]; then
+	mv $SEL_PATH/config $SEL_PATH/config.bk; grep -v '^SELINUX=enforcing' > $SEL_PATH/config.bk $SEL_PATH/config; echo SELINUX=disabled >> $SEL_PATH/config;
+fi
+setenforce 0
